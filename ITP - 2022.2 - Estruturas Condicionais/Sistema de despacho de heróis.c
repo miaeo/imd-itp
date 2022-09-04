efd084bb931d73594e5dbfe50572b19c0b246435
miaeo <112159202+miaeo@users.noreply.github.com>
2022-09-03 22:38:29 -0300

    Update and rename [ITP] Sistema de despacho de heróis to Sistema de despacho de heróis.c

--- ITP - 2022.2 - Estruturas Condicionais/Sistema de despacho de heróis.c
+++ ITP - 2022.2 - Estruturas Condicionais/Sistema de despacho de heróis.c
@@ -1,7 +1,6 @@
-*/[ITP] Sistema de despacho de heróis
+/*[ITP] Sistema de despacho de heróis
 
 A associação de heróis decidiu criar um sistema para minimizar as baixas de seus membros no combate a monstros. Ocasionalmente, um grupo de heróis mais fraco acabava sendo enviado para enfrentar um monstro muito forte, e a associação perdia muitos membros de seus rankings. Para isso, a associação resolveu avaliar a classificação dos heróis e o nível de ameaça dos monstros, e a partir de uma análise minuciosa de dados feita por um de seus heróis, Kid Emperor, criou regras para o envio das equipes.
-
 
 A classificação dos heróis quanto a força é feita de acordo com a seguinte tabela:
 1 - Heróis classe C
@@ -10,14 +9,12 @@ A classificação dos heróis quanto a força é feita de acordo com a seguinte 
 4 - Heróis classe S
 5 - Saitama
 
-
 Já a classificação dos monstros em nível de ameaça é feita de acordo com a seguinte tabela:
 1 - Monstro nível Lobo (ameaça pequenos grupos de pessoas)
 2 - Monstro nível Tigre (ameaça um grande número de pessoas)
 3 - Monstro nível Demônio (pode destruir uma cidade inteira)
 4 - Monstro nível Dragão (pode destruir um continente inteiro)
 5 - Monstro nível Divindade (pode exterminar a raça humana)
-
 
 Observando esses dois grupos, Kid Emperor percebeu que:
 - Para eliminar uma ameaça do nível Lobo, é necessário um herói classe B ou acima, ou pelo menos 3 heróis classe C
@@ -26,9 +23,7 @@ Observando esses dois grupos, Kid Emperor percebeu que:
 - Para eliminar uma ameaça do nível Dragão, é necessário Saitama, ou pelo menos 3 heróis classe S
 - Para eliminar uma ameaça do nível Divindade, apenas Saitama consegue lidar.
 
-
 Como Kid Emperor é um herói classe S e muito ocupado, ele não teve tempo de implementar o algoritmo que faz a análise da ameaça e diz se a equipe escolhida vai vencer ou não o monstro. Por isso, a associação escolheu você para essa tarefa.
-
 
 O seu programa vai receber 3 valores inteiros, na seguinte ordem:
 - Quantidade de heróis que serão enviados
