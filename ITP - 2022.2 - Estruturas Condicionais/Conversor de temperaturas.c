e806d3d518b57eb21b507a7f767f9e21f7ad31e8
miaeo <112159202+miaeo@users.noreply.github.com>
2022-09-03 22:36:35 -0300

    Update and rename [ITP] Conversor de temperaturas to Conversor de temperaturas.c

--- ITP - 2022.2 - Estruturas Condicionais/Conversor de temperaturas.c
+++ ITP - 2022.2 - Estruturas Condicionais/Conversor de temperaturas.c
@@ -1,18 +1,14 @@
 /*[ITP] Conversor de temperaturas
 
 Escreva um programa em que o usuário informe a temperatura (em números reais) e a escala utilizada (Celsius, Farenheit ou Kelvin). 
-
 A saída do programa será a temperatura nas 3 escalas, com duas casas decimais de precisão.
-
 Considere que as escalas serão informadas como uma única letra:
 
 C = Temperatura em graus Celsius (°C)
 F = Temperatura em graus Farenheit (°F)
 K - Temperatura em graus Kelvin (K)
 
-
 A conversão entre as escalas pode ser feita com as seguintes equações:
-
 K = C + 273.15
 F = C * 1.8 + 32*/
 
